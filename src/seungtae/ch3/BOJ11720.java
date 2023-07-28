e5e67f869ded4494a75d18446c8cb7f512d64088
LST <dev.st1027@gmail.com>
2023-07-28 11:10:02 +0900

    숫자 합 BOJ11720

--- src/seungtae/ch3/BOJ11720.java
+++ src/seungtae/ch3/BOJ11720.java
@@ -1,4 +1,23 @@
 package seungtae.ch3;
 
+import java.util.Scanner;
+
 public class BOJ11720 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int count = scanner.nextInt();
+        String num = scanner.next();
+        scanner.close();
+
+        int sum=0;
+
+        for(int i=0; i<count; i++) {
+            sum += num.charAt(i)-'0';
+        }
+
+        System.out.println(sum);
+    }
+
+
 }
