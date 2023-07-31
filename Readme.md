c889c20f2a8a6ea28263db807bafdcf06390bc68
LST <dev.st1027@gmail.com>
2023-07-31 10:40:58 +0900

    평균 BOJ1546

---
+++ Readme.md
@@ -0,0 +1,17 @@
+# Java Coding Test 
+
+## 참여자
+
+- 설주연
+- 이승태
+
+## 문제 목록
+
+1. 숫자의 합 : https://www.acmicpc.net/problem/11720
+2. 평균 : https://www.acmicpc.net/problem/1546
+3. 구간 합 구하기 4 : https://www.acmicpc.net/problem/11659
+4. 구간 합 구하기 2 : https://www.acmicpc.net/problem/10999
+5. 나머지 합 구하기 : https://www.acmicpc.net/problem/10986
+
+---
+
---
+++ src/seungtae/ch3/BOJ1546.java
@@ -0,0 +1,39 @@
+package seungtae.ch3;
+
+import java.util.Scanner;
+
+public class BOJ1546 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int subjectNUM = scan.nextInt();  // 개행문자
+        scan.nextLine();
+
+//        String[] str = scan.nextLine().split(" ");
+        int[] scoreArr = new int[subjectNUM];
+
+        for(int i=0; i<subjectNUM; i++) {
+//            scoreArr[i] = Integer.valueOf(str[i]);
+            scoreArr[i] = scan.nextInt();
+        }
+
+        scan.close();
+
+        int max = 0;
+        double sum = 0;
+        double result = 0;
+        for(int i=0; i<scoreArr.length; i++) {
+            if(max<=scoreArr[i]) {
+                max = scoreArr[i];
+            }
+            sum += scoreArr[i];
+        }
+
+
+        result = (sum*100/max/scoreArr.length); // int (4) => 강제 형변환 double (8)
+
+        System.out.println(result);
+//        System.out.println(sum/max*100/scoreArr.length);
+
+    }
+}
