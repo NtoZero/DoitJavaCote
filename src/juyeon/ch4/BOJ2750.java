ff17c4850e4cbb7e3711162c69034a1173917040
seoljuyeon <sxxljxxxxn@gmail.com>
2023-08-14 21:59:34 +0900

    :poop: 선택 정렬 어렵따!

--- src/juyeon/ch4/BOJ2750.java
+++ src/juyeon/ch4/BOJ2750.java
@@ -5,42 +5,41 @@ import java.util.*;
 // https://www.acmicpc.net/problem/2750
 public class BOJ2750 {
     public static void main(String[] args) {
-        /*
-        1. Arrays.sort() 로 짱 쉽게 풀기
 
-        Scanner sc = new Scanner(System.in);
+//        1. Arrays.sort() 로 짱 쉽게 풀기
+//
+//        Scanner sc = new Scanner(System.in);
+//
+//        int N = sc.nextInt();
+//
+//        int[] arr = new int[N];
+//        for( int i = 0; i < N; i++ ) {
+//            arr[i] = sc.nextInt();
+//        }
+//
+//        Arrays.sort(arr);
+//
+//        for( int i = 0; i < N; i++ ) {
+//            System.out.println(arr[i]);
+//        }
 
-        int N = sc.nextInt();
 
-        int[] arr = new int[N];
-        for( int i = 0; i < N; i++ ) {
-            arr[i] = sc.nextInt();
-        }
+//        2. 우선순위 큐로 풀기
+//
+//        Scanner sc = new Scanner(System.in);
+//
+//        int N = sc.nextInt();
+//
+//        PriorityQueue<Integer> Q = new PriorityQueue<>();
+//
+//        for(int i = 0; i < N; i++ ) {
+//            Q.add(sc.nextInt());
+//        }
+//
+//        for( int i = 0; i < N; i++ ) {
+//            System.out.println(Q.poll());
+//        }
 
-        Arrays.sort(arr);
-
-        for( int i = 0; i < N; i++ ) {
-            System.out.println(arr[i]);
-        }
-         */
-
-        /*
-        2. 우선순위 큐로 풀기
-
-        Scanner sc = new Scanner(System.in);
-
-        int N = sc.nextInt();
-
-        PriorityQueue<Integer> Q = new PriorityQueue<>();
-
-        for(int i = 0; i < N; i++ ) {
-            Q.add(sc.nextInt());
-        }
-
-        for( int i = 0; i < N; i++ ) {
-            System.out.println(Q.poll());
-        }
-         */
 
         // 3. 버블 정렬로 풀기
         // 버블 정렬은 O(n^2) 인데, 해당 문제 최대 범위가 1,000 이라서 가능
