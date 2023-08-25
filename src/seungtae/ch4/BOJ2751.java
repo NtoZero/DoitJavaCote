18827f5b15b96fc598aa3cd329a42850c399cf56
LST <dev.st1027@gmail.com>
2023-08-25 23:36:56 +0900

    feat: BOJ2751 (병합정렬)

---
+++ src/seungtae/ch4/BOJ2751.java
@@ -0,0 +1,79 @@
+package seungtae.ch4;
+
+//20. 수 정렬하기2 (2751) : https://www.acmicpc.net/problem/2751
+
+import java.io.*;
+import java.util.Scanner;
+
+public class BOJ2751 {
+    /*
+        시간복잡도 : N의 최대 개수 10^6 => NlogN의 알고리즘을 사용하자.
+        - 풀이 방식 : 병합정렬
+     */
+
+    public static int[] arr; // 정리할 배열
+    public static int[] tmp; // 임시 배열
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+
+        arr = new int[N+1];
+        tmp = new int[N+1]; // 정렬용 임시 배열
+
+        for(int i=1; i<N+1; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        mergeSort(1, N); //병합정렬 수행
+        for(int i = 1; i < N+1; i++) {
+            bw.write(arr[i]+"\n");
+        }
+        bw.flush();
+        bw.close();
+
+    }
+
+    private static void mergeSort(int start, int end) {
+        if(end - start < 1) return; //탈출 조건
+
+        int middle = start + (end - start)/2; // 중간값 구하기
+
+        mergeSort(start, middle);
+        mergeSort(middle+1, end);
+        for(int i = start; i <= end; i++) {
+            tmp[i] = arr[i];
+        }
+
+        int newIdx = start; // 새로운 배열에 순차적으로 복사할 인덱스
+        int point1 = start;   // 포인트1 조정
+        int point2 = middle+1; // 포인트2 조정
+
+        // 분할된 두 그룹의 포인트가 가리키는 값을 비교해 더 작은 수를 배열에 저장, 선택 데이터 그룹의 point를 오른쪽 한 칸 이동
+        while(point1 <= middle && point2 <= end) {
+            if(tmp[point1] > tmp[point2]) {
+                arr[newIdx] = tmp[point2];
+                newIdx++;
+                point2++;
+            } else if(tmp[point1]<=tmp[point2]) {
+                arr[newIdx] = tmp[point1];
+                newIdx++;
+                point1++;
+            }
+        }
+
+        // 한 그룹 모두 선택된 후 남아있는 값 정리
+        while(point1 <= middle) {
+            arr[newIdx] = tmp[point1];
+            newIdx++;
+            point1++;
+        }
+        while(point2 <= end) {
+            arr[newIdx] = tmp[point2];
+            newIdx++;
+            point2++;
+        }
+    }
+
+}
