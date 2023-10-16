1beb6ea5e0c58e60b97eac8e440a911f32b1ebb7
seoljuyeon <sxxljxxxxn@gmail.com>
2023-10-16 19:54:48 +0900

    Q5 10986 나머지합

---
+++ src/juyeon/review/BOJ10986_RE.java
@@ -0,0 +1,45 @@
+package juyeon.review;
+
+import problems.ch3_data_structure.ch3_1_sumNumbers.P5_10986;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ10986_RE implements P5_10986 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        // 구간합의 나머지 배열
+        long[] S = new long[N+1];
+
+        // M으로 나누어떨어지는 (i,j) 개수
+        int count = 0;
+
+       // 나머지값 같은 거 개수 세는 배열
+        int[] idx = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            S[i] = ( S[i-1] + Long.parseLong(st.nextToken()) ) % M;
+
+            // 0인 애들은 따로 세어주고
+            if (S[i] == 0) count++;
+
+            // 나머지값 같은 거 카운트
+            idx[(int) S[i]]++;
+        }
+
+        // 조합 nCr ( n개 중에 r개 뽑기 ) = ( n * n-1 ) / r
+        for (int i = 0; i < M; i++) {
+            count += ( idx[i] * ( idx[i] - 1 ) ) / 2;
+        }
+
+        System.out.println(count);
+    }
+}
