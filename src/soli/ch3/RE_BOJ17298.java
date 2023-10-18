3e8c9404562d8cf9de4783f173ea89a07f430e5a
luminousol <luminousolding@gmail.com>
2023-10-18 23:53:26 +0900

    12번_BOJ17298. 오큰수

---
+++ src/soli/ch3/RE_BOJ17298.java
@@ -0,0 +1,10 @@
+package soli.ch3;
+
+import problems.ch3_data_structure.ch3_5_stack_queue.P12_17298;
+
+public class RE_BOJ17298 implements P12_17298 {
+    public static void main(String[] args) {
+
+    }
+
+}
