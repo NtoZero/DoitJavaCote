86574809ea01893c70ca1fb2607c46913433f52d
LST <dev.st1027@gmail.com>
2023-10-22 17:58:01 +0900

    :sparkles: feat: 15번_BOJ2750.버블정렬_인접

--- src/seungtae/ch4/S15_BOJ2750_RE.java
+++ src/seungtae/ch4/S15_BOJ2750_RE.java
@@ -7,7 +7,7 @@ import java.io.*;
 public class S15_BOJ2750_RE implements P15_2750 {
     /*
         버블정렬
-        - 서로 인접한 두 수를 swap하며 정렬된 영역을 늘려가는 정렬
+        - 서로 '인접'한 두 수를 swap하며 정렬된 영역을 늘려가는 정렬
         - 주의점 : 앞에서 정해진 차순을 세워서 영역을 늘릴 것인지, 뒤에서부터 늘릴 것인지 확정해야한다.
      */
     public static void main(String[] args) throws IOException {
@@ -21,13 +21,13 @@ public class S15_BOJ2750_RE implements P15_2750 {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        for(int i=0; i<N; i++) {
-            for(int j=i+1; j<N; j++) { // 처음 영역부터 정렬된 영역을 하나씩 늘려 나간다.
+        for(int i=0; i<N-1; i++) {
+            for(int j=0; j<N-1-i; j++) { // 처음 영역부터 정렬된 영역을 하나씩 늘려 나간다.
                 //swap
-                if(arr[i]>arr[j]) {
-                    int tmp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = tmp;
+                if(arr[j]>arr[j+1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
                 }
             }
         }
@@ -38,3 +38,18 @@ public class S15_BOJ2750_RE implements P15_2750 {
         bw.close();
     }
 }
+
+
+/*
+        - 다음은 서로 인접한 수를 비교하지 않았으므로 버블정렬이라고 보기 어렵다.
+        for(int i=0; i<N; i++) {
+            for(int j=i+1; j<N; j++) { // 처음 영역부터 정렬된 영역을 하나씩 늘려 나간다.
+                //swap
+                if(arr[i]>arr[j]) {
+                    int tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
+ */
