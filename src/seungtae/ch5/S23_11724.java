b46288930abd2f6226f691dde0c36362a79589ed
LST <dev.st1027@gmail.com>
2023-11-01 23:18:11 +0900

    :sparkles: feat: 23번_BOJ11724.연결 요소의 개수 (풀이 추가)

--- src/seungtae/ch5/S23_11724.java
+++ src/seungtae/ch5/S23_11724.java
@@ -18,8 +18,11 @@ public class S23_11724 implements P23_11724 {
                 1. DFS를 시작할 노드를 정한 후 사용할 자료구조 초기화
                     1) 인접 리스트로 그래프 표현
                     2) 방문 배열 초기화
-                    3) 시작 노드 스택에 삽입
+                    3) 시작 노드 스택에 삽입 (스택 자료구조에 시작점을 더하기)
                 2. 스택에서 노드를 꺼낸 후 꺼낸 노드의 인접 노드를 다시 스택에 삽입
+                    1) 스택에서 노드를 꺼내면서 탐색 순서에 꺼낸 노드를 기록
+                    2) 인접 리스트, 대상 노드의 인접 노드를 스택에 삽입
+                    3) 노드를 삽입하며 방문 배열 체크
                 3. 스택 자료구조에 값이 없을 때까지 반복
                     1) 인접 노드가 없을 때까지 방문하며, 인접 노드가 존재하더라도 방문했으면 다시 방문하지 않는다.
                         - 방문할 필요가 없다면, 방문할 예정 스택에 삽입하지 않는다.
@@ -27,7 +30,7 @@ public class S23_11724 implements P23_11724 {
                 - O(N+E) : Node 수, Edge 수
      */
 
-    static ArrayList<Integer>[] A; // 인접 그래프
+    static ArrayList<Integer>[] A; // 인접 그래프 (ArrayList의 배열)
     static boolean[] visited; // 방문 여부
     public static void main(String[] args) throws IOException {
         /*
@@ -50,6 +53,15 @@ public class S23_11724 implements P23_11724 {
         for(int i=1; i<=N; i++) {
             A[i] = new ArrayList<Integer>();
         }
+        /*
+            Node에 대한 연결리스트 초기화 (1~N)
+            1
+            2
+            3
+            4
+            5
+            6
+         */
 
         // 2. 간선 수 만큼 인접 리스트 대입
         for(int i=0; i<E; i++) {
@@ -61,6 +73,20 @@ public class S23_11724 implements P23_11724 {
             A[node].add(near);
             A[near].add(node);
         }
+        /*
+            인접 리스트
+                1   2,5
+                2   1,5
+                3   4
+                4   3,6
+                5   2,1
+                6   4
+            - 방문 배열
+                1   2   3   4   5   6
+                F   F   F   F   F   F
+              1에서 시작한 경우
+                T   T   F   F   T   F
+         */
 
         for (int i=1; i<=N; i++) {
             if(!visited[i]) {   // 방문X 노드 없을 때까지 반복
@@ -80,6 +106,7 @@ public class S23_11724 implements P23_11724 {
 
         // 방문 전적 없다면 true
         visited[v] = true;
+            //인접 리스트의 배열 순회
         for (int i : A[v]) {
             // 연결 노드 중 방문하지 않았던 노드만 탐색
             if(visited[i] == false) {
