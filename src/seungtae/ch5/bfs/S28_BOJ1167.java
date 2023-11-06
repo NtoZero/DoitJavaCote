3929b10a66b8ad72a9f5a89cee0395446e3bd083
LST <dev.st1027@gmail.com>
2023-11-06 21:22:30 +0900

    :pencil2:  feat: 28번_BOJ1167.트리의 지름 (초안)

--- src/seungtae/ch5/bfs/S28_BOJ1167.java
+++ src/seungtae/ch5/bfs/S28_BOJ1167.java
@@ -3,4 +3,24 @@ package seungtae.ch5.bfs;
 import _problems.ch5_searching.ch5_2_bfs.P28_1167;
 
 public class S28_BOJ1167 implements P28_1167 {
+    /*
+        알고리즘 : BFS (너비 우선 탐색)
+            - 요구
+                1. 방문 배열
+                2. 그래프 (인접 리스트 배열) OR 2차원 배열
+                3. 큐 (FIFO)
+            - 절차
+                1. BFS를 시작할 노드를 정한 후 사용할 자료구조 초기화하기
+                2. 큐에서 노드를 꺼낸 후 꺼낸 노드의 인접 노드를 다시 큐에 삽입
+                3. 큐에 노드가 존재하지 않을때까지 반복
+     */
+    /*
+        정점의 개수 N (10^3)
+        간선의 개수 M (10^4)
+        탐색을 시작할 정점의 번호 V
+
+     */
+    public static void main(String[] args) {
+
+    }
 }
