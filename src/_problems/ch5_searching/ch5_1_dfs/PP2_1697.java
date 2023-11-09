b6634e5552ccda5ffa53bc047afe7d96ff177455
LST <dev.st1027@gmail.com>
2023-11-09 11:00:18 +0900

    :pencil2: chore: 추가 문제 2번 변경 2178 -> 1697

---
+++ src/_problems/ch5_searching/ch5_1_dfs/PP2_1697.java
@@ -0,0 +1,5 @@
+package _problems.ch5_searching.ch5_1_dfs;
+
+public interface PP2_1697 {
+    //    https://www.acmicpc.net/problem/1697
+}
--- src/_problems/ch5_searching/ch5_1_dfs/PP2_2178.java
+++
@@ -1,5 +0,0 @@
-package _problems.ch5_searching.ch5_1_dfs;
-
-public interface PP2_2178 {
-    //    https://www.acmicpc.net/problem/2178
-}
