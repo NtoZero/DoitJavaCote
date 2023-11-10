43a2f88052c6d9833d6f6448218aa2f2e0c920cd
luminousol <luminousolding@gmail.com>
2023-11-10 14:11:31 +0900

    ✨feat: DFS 문제 인터페이스 추가

---
+++ src/_problems/bonus/DFS_2178.java
@@ -0,0 +1,5 @@
+package _problems.bonus;
+
+public interface DFS_2178 {
+    // https://www.acmicpc.net/problem/2178
+}
---
+++ src/_problems/bonus/DFS_2606.java
@@ -0,0 +1,5 @@
+package _problems.bonus;
+
+public interface DFS_2606 {
+    // https://www.acmicpc.net/problem/2606
+}
