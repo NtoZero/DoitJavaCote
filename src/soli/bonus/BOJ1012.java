6443f2418d7719a528d8d57b51aee1fe76848be6
luminousol <luminousolding@gmail.com>
2023-11-15 21:00:21 +0900

    ♻refactor: BOJ1012.BFS 추가

--- src/soli/bonus/BOJ1012.java
+++ src/soli/bonus/BOJ1012.java
@@ -5,6 +5,8 @@ import _problems.ch5_searching.ch5_2_bfs.PP1_1012;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BOJ1012 implements PP1_1012 {
@@ -42,7 +44,7 @@ public class BOJ1012 implements PP1_1012 {
             for(int i = 0; i < N ; i++) {
                 for(int j = 0 ; j < M ; j++) {
                     if(A[i][j] == 1 && !visited[i][j]) {
-                        dfs(i,j);
+                        bfs(i,j);
                         count++;
                     }
                 }
@@ -54,19 +56,43 @@ public class BOJ1012 implements PP1_1012 {
 
     }
 
-    private static void dfs(int y, int x) {
+    private static void bfs(int y, int x) {
+        Queue<int[]> Q = new LinkedList<>();
+        Q.add(new int[]{y, x});
         visited[y][x] = true;
 
-        for(int i = 0; i < 4; i++) {
-            int ax = y + a[i];
-            int by = x + b[i];
+            while (!Q.isEmpty()) {
+                int[] current = Q.remove();
+                int cy = current[0]; // 현재 세로 위치
+                int cx = current[1]; // 현재 가로 위치
 
-            if(ax >= 0 && by >=0 && ax < N && by < M ) {
-                if(A[ax][by] == 1 && !visited[ax][by]) {
-                    dfs(ax, by);
+                for(int i = 0; i < 4 ; i++) {
+                    int ny = cy + a[i];
+                    int nx = cx + b[i];
+
+                    if(ny >= 0 && nx >= 0 && ny < N && nx < M) {
+                        if(!visited[ny][nx] && A[ny][nx] == 1) {
+                            visited[ny][nx] = true;
+                            Q.add(new int[]{ny, nx});
+                        }
+                    }
                 }
             }
-
-        }
     }
+
+//    private static void dfs(int y, int x) {
+//        visited[y][x] = true;
+//
+//        for(int i = 0; i < 4; i++) {
+//            int ax = y + a[i];
+//            int by = x + b[i];
+//
+//            if(ax >= 0 && by >=0 && ax < N && by < M ) {
+//                if(A[ax][by] == 1 && !visited[ax][by]) {
+//                    dfs(ax, by);
+//                }
+//            }
+//
+//        }
+//    }
 }
