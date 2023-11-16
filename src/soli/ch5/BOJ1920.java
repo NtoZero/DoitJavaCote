39395674194ffcfaefdf23b805ee3d0553ef2f0d
luminousol <luminousolding@gmail.com>
2023-11-16 23:16:52 +0900

    ✨feat: BOJ1920.원하는 정수 찾기

---
+++ src/soli/ch5/BOJ1920.java
@@ -0,0 +1,9 @@
+package soli.ch5;
+
+import _problems.ch5_searching.ch5_3_binary_search.P29_1920;
+
+public class BOJ1920 implements P29_1920 {
+    public static void main(String[] args) {
+
+    }
+}
