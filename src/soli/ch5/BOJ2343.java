29fe0bc4cf35af50941e7a4247d7f28ce0b5c0c5
luminousol <luminousolding@gmail.com>
2023-11-16 23:17:19 +0900

    ✨feat: BOJ2343.블루레이 만들기

---
+++ src/soli/ch5/BOJ2343.java
@@ -0,0 +1,9 @@
+package soli.ch5;
+
+import _problems.ch5_searching.ch5_3_binary_search.P30_2343;
+
+public class BOJ2343 implements P30_2343 {
+    public static void main(String[] args) {
+
+    }
+}
