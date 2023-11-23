e7821055f53dd59a94df7dd785e7076ea2f62d85
luminousol <luminousolding@gmail.com>
2023-11-23 23:46:13 +0900

    ✨feat: BOJ1929.소수 구하기

---
+++ src/soli/ch7/BOJ1929.java
@@ -0,0 +1,9 @@
+package soli.ch7;
+
+import _problems.ch7_integer.ch7_1_prime_number.P37_1929;
+
+public class BOJ1929 implements P37_1929 {
+    public static void main(String[] args) {
+
+    }
+}
