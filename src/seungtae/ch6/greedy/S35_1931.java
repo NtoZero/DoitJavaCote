79c60ddb9303460d342aabc7cf623ba752a144ac
LST <dev.st1027@gmail.com>
2023-11-25 13:15:12 +0900

    :sparkles: feat:BOJ1931.회의실 배정 & BOJ1541.잃어버린 괄호

--- src/seungtae/ch6/greedy/S35_1931.java
+++ src/seungtae/ch6/greedy/S35_1931.java
@@ -14,35 +14,57 @@ public class S35_1931 implements P35_1931 {
 
         문제 풀이 :
             1.우선순위 큐로 정렬.
-                1) 기본적으로 시작 시간이 적은 순으로 정렬
-                2) 시작 시간이 같으면 끝-시간 차가 적은 것 우선
+                1) 기본적으로 종료 시간이 적은 순으로 정렬
+                2) 종료 시간이 같으면 시작 시간이 적은 순 (갭이 적은 순)
 
-            2. 정렬된 상태에서 시작 시간이 같은
+            2. 정렬된 상태에서 시작 시간이 같은 것들을 건너뛰고
+                끝 시간 이후인 시작시간을 가지고 있는 Pair 객체를 카운트
 
 
      */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+        int result = 0;
 
         List<Pair> pairs = new ArrayList<>();
         for(int i=0; i<N; i++) {
             pairs.add(new Pair(br.readLine()));
         }
 
+        Collections.sort(pairs, new Comparator<Pair>() {
+            public int compare(Pair o1, Pair o2) {
+//                if(o1.end != o2.end) {
+//                    return o1.end-o2.end;
+//                }
+//                return o1.start-o2.start;
+                if(o1.end == o2.end) return o1.start-o2.start;
+                return o1.end-o2.end;
+            }
+        });
 
+        int beforeStart = -1;
+        int beforeEnd = -1;
+        for(Pair p : pairs) {
+//            if(beforeStart == p.start) continue;
+            if(p.start >= beforeEnd) { // 시작과 종료가 같은 시간에 끝나는 경우 ex) 2 2 를 고려하여 등호 포함
+                beforeStart = p.start;
+                beforeEnd = p.end;
+                System.out.println(beforeStart + " " + beforeEnd);
+                result++;
+            }
+        }
+        System.out.println(result);
     }
 
     static class Pair {
         int start;
         int end;
-        int gap;
 
         public Pair(String line) {
             String[] numbers = line.split(" ");
             this.start = Integer.parseInt(numbers[0]);
             this.end = Integer.parseInt(numbers[1]);
-            this.gap = Math.abs(start - end);
         }
     }
 }
