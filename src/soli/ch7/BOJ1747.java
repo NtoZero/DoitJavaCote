5d8c3751d39e3e94d38a3274295465f3950d231f
luminousol <luminousolding@gmail.com>
2023-11-29 00:56:57 +0900

    ✨feat: BOJ1747.소수 & 팰린드롬

---
+++ src/soli/ch7/BOJ1747.java
@@ -0,0 +1,9 @@
+package soli.ch7;
+
+import _problems.ch7_integer.ch7_1_prime_number.P39_1747;
+
+public class BOJ1747 implements P39_1747 {
+    public static void main(String[] args) {
+
+    }
+}
