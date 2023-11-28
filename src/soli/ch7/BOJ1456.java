f72da22f6932006fd85aa93349137b15a5289af5
luminousol <luminousolding@gmail.com>
2023-11-29 00:56:23 +0900

    ✨feat: BOJ1456.거의 소수

---
+++ src/soli/ch7/BOJ1456.java
@@ -0,0 +1,9 @@
+package soli.ch7;
+
+import _problems.ch7_integer.ch7_1_prime_number.P38_1456;
+
+public class BOJ1456 implements P38_1456 {
+    public static void main(String[] args) {
+
+    }
+}
