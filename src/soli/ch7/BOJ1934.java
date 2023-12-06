03b4db26b3a209a2c11ba799b0c8a81b215dd92e
luminousol <luminousolding@gmail.com>
2023-12-06 23:13:51 +0900

    ✨feat: BOJ1934.최소공배수 구하기

---
+++ src/soli/ch7/BOJ1934.java
@@ -0,0 +1,12 @@
+package soli.ch7;
+
+import _problems.ch7_integer.ch7_3_euclidean.P42_1934;
+
+// 유클리드 호제
+// 최소공배수
+
+public class BOJ1934 implements P42_1934 {
+    public static void main(String[] args) {
+
+    }
+}
