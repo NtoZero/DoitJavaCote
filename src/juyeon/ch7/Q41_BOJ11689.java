131bbce6df6c51199a8995d4ed3328b502c5a323
seoljuyeon <sxxljxxxxn@gmail.com>
2023-12-06 17:36:53 +0900

    ✨feat: 41번_BOJ11689.오일러_피_함수

---
+++ src/juyeon/ch7/Q41_BOJ11689.java
@@ -0,0 +1,49 @@
+package juyeon.ch7;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+// ❌❌❌ 런타임 에러 ( long 타입으로 받아야 되는데, long으로 받아도 배열 크기가 long 범위를 벗어나서 안 댐)
+public class Q41_BOJ11689 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            arr[i] = i;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+
+        // n 약수
+        for (int i = 2; i <= n; i++) {
+            if (n % i == 0) {
+                q.add(i);
+            }
+        }
+
+        while (!q.isEmpty()) {
+            int e = q.poll();
+            for (int i = 1; i <= n; i++) {
+                if (arr[i] % e == 0) {
+                    arr[i] = 0;
+                }
+            }
+        }
+
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (arr[i] != 0) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+
+    }
+}
---
+++ src/juyeon/ch7/Q41_BOJ11689_2.java
@@ -0,0 +1,30 @@
+package juyeon.ch7;
+
+import java.util.Scanner;
+
+// 시간 초과
+public class Q41_BOJ11689_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+
+        int count = 0;
+
+        for (long i = 1; i <= n; i++) {
+            if (isGcd(n, i) == 1) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+
+    private static long isGcd(long a, long b) {
+        if (b == 0) {
+            return a;
+        }
+        return isGcd(b, a % b);
+    }
+
+}
