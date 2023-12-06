9dedc1e64fb62e8e58aba8cf308254d9ee46bcc9
seoljuyeon <sxxljxxxxn@gmail.com>
2023-12-06 17:37:24 +0900

    ✨feat: 43번_BOJ1850.최대_공약수

---
+++ src/juyeon/ch7/Q43_BOJ1850.java
@@ -0,0 +1,35 @@
+package juyeon.ch7;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q43_BOJ1850 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+
+        for (int i = 1; i <= gcd(A, B); i++) {
+            sb.append(1);
+        }
+
+        System.out.println(sb.toString());
+
+
+    }
+
+    private static long gcd(long A, long B) {
+        while (A != 0) {
+            long r = B % A;
+            B = A;
+            A = r;
+        }
+        return B;
+    }
+}
