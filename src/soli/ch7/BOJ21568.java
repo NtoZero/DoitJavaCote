72e7239780fade56048b07a5b6af2a59e366ae79
luminousol <luminousolding@gmail.com>
2023-12-06 23:16:39 +0900

    ✨feat: BOJ21568. Ax + By = C

---
+++ src/soli/ch7/BOJ21568.java
@@ -0,0 +1,9 @@
+package soli.ch7;
+
+import _problems.ch7_integer.ch7_4_extended_euclidean.P45_21568;
+
+public class BOJ21568 implements P45_21568 {
+    public static void main(String[] args) {
+
+    }
+}
