a4c2332ba367295f09bf8c6d7ad3d389d901909d
luminousol <luminousolding@gmail.com>
2023-12-06 23:14:04 +0900

    ✨feat: BOJ1934.최대공약수 구하기

---
+++ src/soli/ch7/BOJ1850.java
@@ -0,0 +1,10 @@
+package soli.ch7;
+
+import _problems.ch7_integer.ch7_3_euclidean.P43_1850;
+
+// 최대공약수 구하기
+public class BOJ1850 implements P43_1850 {
+    public static void main(String[] args) {
+
+    }
+}
