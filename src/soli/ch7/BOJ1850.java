bd93a256158d02ab2d02d3bb0408ce4fa0ee3295
luminousol <luminousolding@gmail.com>
2023-12-10 22:21:56 +0900

    ✨feat: BOJ1850.최대공약수 구하기

--- src/soli/ch7/BOJ1850.java
+++ src/soli/ch7/BOJ1850.java
@@ -2,9 +2,36 @@ package soli.ch7;
 
 import _problems.ch7_integer.ch7_3_euclidean.P43_1850;
 
+import java.io.*;
+import java.util.StringTokenizer;
+
 // 최대공약수 구하기
 public class BOJ1850 implements P43_1850 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+
+        long result = gcd(A, B);
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        while (result > 0) {
+            bw.write("1");
+            result--;
+        }
+        bw.flush();
+        bw.close();
+    }
+
+    private static long gcd(long a, long b) {
+        if(b==0) {
+            return a;
+        }
+        else {
+            return gcd(b, a%b);
+        }
     }
 }
