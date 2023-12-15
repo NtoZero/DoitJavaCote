8f2b271ca18c2786fc3ebf7f3617bbe0a617602e
luminousol <luminousolding@gmail.com>
2023-12-15 10:07:01 +0900

    ✨feat: BOJ1325. 효율적으로 해킹하기

--- src/soli/ch8/BOJ1325_bfs.java
+++ src/soli/ch8/BOJ1325_bfs.java
@@ -2,8 +2,86 @@ package soli.ch8;
 
 import _problems.ch8_graph.P47_1325;
 
-public class BOJ1325_bfs implements P47_1325 {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
+public class BOJ1325_bfs implements P47_1325 {
+
+    // 배열 리스트
+    static ArrayList<Integer>[] arr;
+
+    // 신뢰관계 방문 저장하는 배열
+    static int[] count;
+
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // 컴퓨터 대수(노드수)
+        int N = Integer.parseInt(st.nextToken());
+        // 신뢰하는 관계(엣지수 -> 반복횟수)
+        int M = Integer.parseInt(st.nextToken());
+
+        arr = new ArrayList[N+1];
+
+        for(int i = 1 ; i <= N ; i++) {
+            arr[i] = new ArrayList<>();
+        }
+
+        for(int i = 0 ; i < M ; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            arr[A].add(B);
+        }
+
+        count = new int[N+1];
+
+        for(int i = 1; i <= N ; i++) {
+            visited = new boolean[N+1];
+            bfs(i);
+        }
+
+        int max= 0;
+
+        for(int i = 1 ; i <= N ; i++) {
+            // bfs 탐색하고 나서 max값 찾기
+            max = Math.max(max, count[i]);
+        }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        for(int i = 1; i<= N ; i++) {
+            if(max == count[i]) {
+                bw.write(i + " ");
+                bw.close();
+            }
+        }
+    }
+
+    private static void bfs(int start) {
+        visited[start] = true;
+
+        Queue<Integer> Q = new LinkedList<>();
+        Q.add(start);
+
+        while(!Q.isEmpty()) {
+            int current = Q.poll();
+
+            for(int i : arr[current]) {
+                if(!visited[i]) {
+                    // 방문 배열 count++
+                    count[i]++;
+                    visited[i] = true;
+                    Q.add(i);
+                }
+            }
+        }
     }
 }
