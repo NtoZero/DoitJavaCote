48969d2f6668e70ca0bb643336c0bab818f7ce25
luminousol <luminousolding@gmail.com>
2024-01-03 00:22:21 +0900

    ✨feat: BOJ1043. 거짓말

---
+++ src/soli/ch8/BOJ1043_2.java
@@ -0,0 +1,10 @@
+package soli.ch8;
+
+import _problems.ch8_graph.P52_1043;
+
+// 시간 남으면.. 유니온 파인드로 풀어볼 예정..
+public class BOJ1043_2 implements P52_1043 {
+    public static void main(String[] args) {
+
+    }
+}
