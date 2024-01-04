dc7aa8a88d80e364aba48443266aec16dc859a1b
luminousol <luminousolding@gmail.com>
2024-01-04 23:52:57 +0900

    ✨feat: BOJ2252. 줄세우기

--- src/soli/ch8/BOJ2252.java
+++ src/soli/ch8/BOJ2252.java
@@ -2,8 +2,22 @@ package soli.ch8;
 
 import _problems.ch8_graph.P53_2252;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class BOJ2252 implements P53_2252 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // N 명의 학생들
+        int N = Integer.parseInt(st.nextToken());
+        // 키를 비교한 횟수
+        int M = Integer.parseInt(st.nextToken());
+
+
 
     }
 }
